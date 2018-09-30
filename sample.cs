1cc687d24114f63181ea1e854aca7bca836ba8c0
Yoctobe <gsm.unlock.server.com@gmail.com>
2018-09-30 16:11:33 +0100

    Update sample.cs

--- sample.cs
+++ sample.cs
@@ -1,4 +1,23 @@
-wordToSyllables wTS = new wordToSyllables(); 
-List syllablesList = new List(); 
-syllablesList = (wTS.convertWordToSyllables("erklärte"));
-foreach (string syllable in syllablesList) { Console.WriteLine(syllable); }
+            wordToSyllables wTS = new wordToSyllables() ;
+
+            List<string> syllablesList = new List<string>();
+
+            wTS.Word = "Gesellschaft";
+
+            syllablesList = wTS.Syllables();
+
+            // Display All Syllables
+            foreach (string syllable in syllablesList)
+            {
+                Console.WriteLine(syllable);             
+            }
+
+/* Output: 
+Ge
+se
+llschaft
+Syllables Count : 3
+*/  
+
+            // Count Syllables 
+            Console.WriteLine("Syllables Count : " + syllablesList.Count);
